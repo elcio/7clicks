b26481cfedcbf9458c9e6ae0e17dd86a3aeedb89
Elcio Ferreira <elcio@visie.com.br>
2020-08-27 22:55:20 -0300

    Adding cache

--- setecliques.py
+++ setecliques.py
@@ -1,6 +1,10 @@
+import os
+import hashlib
 import requests
 from flask import Flask
 app = Flask(__name__)
+
+os.makedirs('cache')
 
 @app.route('/')
 def hello_world():
@@ -16,9 +20,17 @@ def artigo(verbete):
     return html
 
 def read_wikipedia(verbete):
-    # TODO: cache
-    request = requests.get(f'https://pt.wikipedia.org/wiki/{verbete}')
+    url = f'https://pt.wikipedia.org/wiki/{verbete}'
+    urlhash = make_hash(url)
+    if os.path.isfile(f'cache/{urlhash}.html'):
+        return open(f'cache/{urlhash}.html').read()
+    request = requests.get(url)
+    with open(f'cache/{urlhash}.html','w') as f:
+        f.write(request.text)
     return request.text
+
+def make_hash(url):
+    return hashlib.md5(url.encode('utf-8')).hexdigest()
 
 def add_custom_header(original_html):
     # TODO: cache
